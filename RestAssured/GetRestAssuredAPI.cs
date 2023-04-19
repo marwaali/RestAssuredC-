e80ef48d6d7e0a7ecfb6827ebf6bc88b838795d8
marwaali <mbassiouny@effvision.com>
2023-04-19 17:51:24 +0200

    add project

---
+++ RestAssured/GetRestAssuredAPI.cs
@@ -0,0 +1,31 @@
+namespace RestAssured
+{
+    public class GetRestAssuredAPI
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var response = Given().When().Get("http://api.zippopotam.us/us/90210");
+            var country = response.Extract().Body("$.['country abbreviation']");
+            Assert.That(country, Is.EqualTo("US"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var response = Given().When().Get("http://api.zippopotam.us/us/90210");
+            var country = response.Extract().Body("$.places[0].state");
+            Assert.That(country, Is.EqualTo("California"));
+        }
+
+
+        [Test]
+        public void Test3()
+        {
+            var response = Given().When().Get("http://api.zippopotam.us/us/90210");
+            var state = response.Extract().Body("$.places[?(@.state=='California')].['state abbreviation']");
+            Assert.That(state, Is.EqualTo("CA"));
+        }
+    }
+}
